f9486e6d199cd9057bd4793d47762244ccd56f28
Dennis Nedry <78676320+mastercodeon314@users.noreply.github.com>
2024-02-27 22:31:14 -0600

    Echos and uninstaller name patches

--- ngrok-adb-installer.sh
+++ ngrok-adb-installer.sh
@@ -1,15 +1,15 @@
-echo "NGROK ADB INSTALLER Updating..."
+echo "[###NGROK ADB INSTALLER###] Updating..."
 pkg update -y && pkg upgrade
 
-echo "NGROK ADB INSTALLER Installing wget proot tar"
+echo "[###NGROK ADB INSTALLER###] Installing wget proot tar"
 pkg install wget proot tar -y
 
-echo "NGROK ADB INSTALLER Going to termux bin directory"
+echo "[###NGROK ADB INSTALLER###] Going to termux bin directory"
 cd /data/data/com.termux/files/usr/bin
 
-echo "NGROK ADB INSTALLER Making uninstaller script"
-touch ./ngrok-adb_uninstall.sh
-cat <<EOT >> ./ngrok-adb_uninstall.sh
+echo "[###NGROK ADB INSTALLER###] Making uninstaller script"
+touch ./ngrok-adb-uninstall.sh
+cat <<EOT >> ./ngrok-adb-uninstall.sh
 # Change directory
 cd /data/data/com.termux/files/usr/bin
 
@@ -20,27 +20,27 @@ rm -rf ./kali-fs
 rm -rf ./start-kali.sh
 echo "Uninstall finished!"
 EOT
-chmod +x ./ngrok-adb_uninstall.sh
+chmod +x ./ngrok-adb-uninstall.sh
 
-echo "NGROK ADB INSTALLER Downloading"
+echo "[###NGROK ADB INSTALLER###] Downloading"
 wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Kali/kali.sh 
 
-echo "NGROK ADB INSTALLER Executing kali installerr script"
+echo "[###NGROK ADB INSTALLER###] Executing kali installerr script"
 chmod 777 ./kali.sh
 ./kali.sh
 rm -rf ./kali.sh
 
-echo "NGROK ADB INSTALLER Navigating to kali-fs/bin"
+echo "[###NGROK ADB INSTALLER###] Navigating to kali-fs/bin"
 cd /data/data/com.termux/files/usr/bin/kali-fs/bin
 
-echo "NGROK ADB INSTALLER Downloading and extracting ngrok to kali-fs/bin"
+echo "[###NGROK ADB INSTALLER###] Downloading and extracting ngrok to kali-fs/bin"
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-arm64.tgz && tar -xzf ngrok-v3-stable-linux-arm64.tgz && rm ngrok-v3-stable-linux-arm64.tgz && chmod 777 ngrok
 
-echo "NGROK ADB INSTALLER Downloading and extracting adb to kali-fs/bin, as well as chmod 777 the binary"
+echo "[###NGROK ADB INSTALLER###] Downloading and extracting adb to kali-fs/bin, as well as chmod 777 the binary"
 wget https://github.com/qhuyduong/arm_adb/releases/download/v1.0.39-aarch64/adb && chmod 777 adb
 
-echo "NGROK ADB INSTALLER Navigating back to termux home directory"
+echo "[###NGROK ADB INSTALLER###] Navigating back to termux home directory"
 cd 
 
-echo "NGROK ADB INSTALLER Ngrok-adb Install complete! Starting kali chroot!"
+echo "[###NGROK ADB INSTALLER###] Ngrok-adb Install complete! Starting kali chroot!"
 start-kali.sh
